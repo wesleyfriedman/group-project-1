d0534612dda64100445550333f265db943c93fa6
wesleyfriedman <friedman.wesley@gmail.com>
2016-10-27 21:52:20 -0400

    add show pages

--- group-project-1/app/models/task.rb
+++ group-project-1/app/models/task.rb
@@ -3,4 +3,10 @@ class Task < ApplicationRecord
 	belongs_to :event
 	has_many :supplies
 	validates :name, presence: true
+
+	def find_event(event_id)
+		event = Event.find(event_id)
+		event.name
+	end
+
 end
