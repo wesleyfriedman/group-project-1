9a7ac7a41567d0db75b1ffb54867e173337777f0
wesleyfriedman <friedman.wesley@gmail.com>
2016-11-01 18:13:53 -0400

    try again fix

--- group-project-1/config/environments/production.rb
+++ group-project-1/config/environments/production.rb
@@ -1,15 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    authentication: "plain",
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "oddter.herokuapp.com",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME_DEV'],
-    password: ENV['GMAIL_PASSWORD_DEV']
-  }
+ config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = { :host => "http://oddter.herokuapp.com/" }
+config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'http://oddter.herokuapp.com/',
+      :user_name => ENV['GMAIL_USERNAME_DEV'],
+      :password => ENV['GMAIL_PASSWORD_DEV'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
