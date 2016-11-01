dd3f84bd66c16ac0ae6f201a3c88b2f1fc23cbbc
wesleyfriedman <friedman.wesley@gmail.com>
2016-11-01 18:27:12 -0400

    change email settings

--- group-project-1/config/environments/production.rb
+++ group-project-1/config/environments/production.rb
@@ -1,16 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
- config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { :host => "http://oddter.herokuapp.com/" }
+config.action_mailer.default_url_options = { :host => 'http://oddter.herokuapp.com/' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => 'http://oddter.herokuapp.com/',
-      :user_name => ENV['GMAIL_USERNAME_DEV'],
-      :password => ENV['GMAIL_PASSWORD_DEV'],
-      :authentication => :plain,
-      :enable_starttls_auto => true
-}
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "http://oddter.herokuapp.com/",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV['GMAIL_USERNAME_DEV'],
+  password: ENV['GMAIL_PASSWORD_DEV']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
